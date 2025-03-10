253d547f3444a863ab92d0a30b3a1b927fbd34af
SergioSanchez666 <L20070523@cdmadero.tecnm.mx>
2025-03-10 11:34:18 -0600

    Primer commit con documentacion

--- app/src/main/java/com/example/calculadora_basica/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_basica/MainActivity.kt
@@ -1,4 +1,7 @@
 package com.example.calculadora_basica
+//Miembros de equipo:
+//Coronel Meza Sergio Daniel
+//Sanchez Cruz Sergio Antonio
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -17,17 +20,35 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculadora_basica.ui.theme.Calculadora_BasicaTheme
 
 class MainActivity : ComponentActivity() {
+
+    /**
+     * EditText para mostrar los números ingresados y el resultado.
+     */
     private lateinit var editTextPantalla: EditText
+
+    /**
+     * Almacena el primer operando de la operación.
+     */
     private var operando1: Double = 0.0
+
+    /**
+     * Almacena el operador de la operación (+, -, *, /).
+     */
     private var operador: Char = ' '
 
+    /**
+     * Método llamado al crear la actividad.
+     * Inicializa la vista, configura los listeners de los botones.
+     */
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicializa el EditText para la pantalla de la calculadora.
         editTextPantalla = findViewById(R.id.editTextPantalla)
 
+        // Array con los IDs de los botones de la calculadora.
         val botones = arrayOf(
             R.id.button0, R.id.button1, R.id.button2, R.id.button3,
             R.id.button4, R.id.button5, R.id.button6, R.id.button7,
@@ -35,6 +56,7 @@ class MainActivity : ComponentActivity() {
             R.id.buttonRestar, R.id.buttonMultiplicar, R.id.buttonDividir, R.id.buttonIgual,
         )
 
+        // Configura los listeners de los botones.
         for (botonId in botones) {
             val boton = findViewById<Button>(botonId)
             boton.setOnClickListener {
@@ -71,11 +93,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    /**
+     * Guarda el operando actual del EditText y limpia el EditText.
+     */
     private fun guardarOperando() {
         operando1 = editTextPantalla.text.toString().toDoubleOrNull() ?: 0.0
         editTextPantalla.text.clear()
     }
 
+    /**
+     * Calcula el resultado de la operación y lo muestra en el EditText.
+     * Reinicia los operandos y el operador para la próxima operación.
+     */
     private fun calcularResultado() {
         val operando2 = editTextPantalla.text.toString().toDoubleOrNull() ?: 0.0
         val resultado = when (operador) {
